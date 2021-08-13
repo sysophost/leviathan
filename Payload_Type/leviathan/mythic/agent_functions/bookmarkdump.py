406d6649cf6faeeb1aea4387289f2b90ded70186
Dean Hardcastle <dean@canva.com>
2021-08-13 16:22:07 +1000

    first add of bookmarkdump command

---
+++ Payload_Type/leviathan/mythic/agent_functions/bookmarkdump.py
@@ -0,0 +1,29 @@
+import json
+
+from mythic_payloadtype_container.MythicCommandBase import *
+
+
+class BookmarkDumpArguments(TaskArguments):
+    def __init__(self, command_line):
+        super().__init__(command_line)
+        self.args = {}
+
+    async def parse_arguments(self):
+        pass
+
+
+class BookmarkDumpCommand(CommandBase):
+    cmd = "bookmarkdump"
+    needs_admin = False
+    help_cmd = "bookmarkdump"
+    description = "Dump all bookmarkdump from the browser"
+    version = 1
+    author = "@sysop_host"
+    argument_class = BookmarkDumpArguments
+    attackmapping = []
+
+    async def create_tasking(self, task: MythicTask) -> MythicTask:
+        return task
+
+    async def process_response(self, response: AgentResponse):
+        pass
---
+++ documentation-payload/leviathan/commands/bookmarkdump.md
@@ -0,0 +1,26 @@
++++
+title = "bookmarkdump"
+chapter = false
+weight = 100
+hidden = false
++++
+
+## Summary
+
+Dump all browser bookmarks
+- Needs Admin: False  
+- Version: 1  
+- Author: @sysop_host  
+
+### Arguments
+
+## Usage
+
+```
+bookmarkdump
+```
+
+
+## Detailed Summary
+This command uses the chrome.bookmark API to obtain all available bookmarks for the current user.
+https://developer.chrome.com/docs/extensions/reference/bookmarks/
